d9f9651da2cc692ddf47b6d3da349cc1011d9842
Lacy, Connor <lacyconnor1409@gmail.com>
2019-12-06 13:45:27 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = require('./routes/route.js')
+var path = require('path');
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+app.use('/', router);
+module.exports=app;
