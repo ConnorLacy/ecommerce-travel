d5f1657e79e98c234d6c9416a7ece4e3bf6598f6
Lacy, Connor <lacyconnor1409@gmail.com>
2019-12-07 12:28:22 -0500

    Add component for creating MongoDB entry

---
+++ client/components/Add.js
@@ -0,0 +1,199 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+import Modal from 'react-modal';
+import {Link} from 'react-router-dom';
+import Axios from "axios"
+import { directive } from '@babel/types';
+
+var querystring = require('querystring');
+
+class Add extends React.Component{
+    constructor(){
+        super();
+    
+        this.state = {
+            year: '',
+            make: '',
+            model: '',
+            description: '',
+            amount: '',
+            availability: true,
+            messageFromServer: ''
+        }
+
+        this.handleSelectChange = this.handleSelectChange.bind(this);
+        this.onClick = this.onClick.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.insertNewRental = this.insertNewRental.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openModal(){
+        this.setState({
+            modalIsOpen: true
+        });
+    }
+
+    closeModal(){
+        this.setState({
+            modalIsOpen: false,
+            year: '',
+            make: '',
+            model: '',
+            description: '',
+            amount: '',
+            availability: true,
+            messageFromServer: ''
+        });
+    }
+
+    componentDidMount(){
+        this.setState({
+            make: this.selectedMake,
+            model: this.props.selectedModel
+        });
+    }
+
+    handleSelectChange(e){
+        if(e.target.name == 'year'){
+            this.setState({
+                year: e.target.value
+            });
+        }
+        if(e.target.name == 'make'){
+            this.setState({
+                make: e.target.value
+            });
+        }
+    }
+
+    onClick(e){
+        this.insertNewRental(this);
+    }
+
+    insertNewRental(e) {
+        Axios.post('/insert',
+        querystring.stringify({
+            year: e.state.year,
+            make: e.state.make,
+            model: e.state.model,
+            description: e.state.description,
+            availability: e.state.availability
+        }), {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).then(function(response){
+            e.setState({
+                messageFromServer: response.data
+            });
+        });
+    }
+
+    handleTextChange(e){
+        if(e.target.name == "description"){
+            this.setState({
+                description: e.target.value
+            });
+        }
+        if(e.target.name == "amount"){
+            this.setState({
+                amount: e.target.value
+            });
+        }
+        if(e.target.name == "model"){
+            this.setState({
+                model: e.target.value
+            })
+        }
+    }
+
+    render(){
+        if(this.state.messageFromServer == ''){
+            return(
+                <div>
+                    <Button variant="success" size="small" onClick={this.openModal}><span className="glyphicon glyphicon-plus"></span></Button>
+                    <Modal
+                        isOpen={this.state.modalIsOpen}
+                        onRequestClose={this.closeModal}
+                        contentLabel="Add Expense"
+                        className="Modal">
+                    <Link to={{pathname: '/', search: '' }} style={{ textDecoration: 'none' }}>
+                        <Button variant="danger" size="mini" onClick={this.closeModal}><span className="closebtn glyphicon glyphicon-remove"></span></Button>
+                    </Link><br/>
+                    <fieldset>
+                        <label htmlFor="year">Year:</label>
+                            <select id="year" name="year" value={this.state.year} onChange={this.handleSelectChange}>
+                                <option value="2020" id="20" selected>2020</option>
+                                <option value="2019" id="20">2019</option>
+                                <option value="2018" id="20">2018</option>
+                                <option value="2017" id="20">2017</option>
+                                <option value="2016" id="20">2016</option>
+                                <option value="2015" id="20">2015</option>
+                                <option value="2014" id="20">2014</option>
+                                <option value="2013" id="20">2013</option>
+                                <option value="2012" id="20">2012</option>
+                                <option value="2011" id="20">2011</option>
+                                <option value="2010" id="20">2010</option>
+                            </select>
+                        <label htmlFor="make">Make:</label>
+                            <select id="make" name="make" value={this.state.make} onChange={this.handleSelectChange}>
+                                <option value="Acura" id="Acura" selected>Acura</option>
+                                <option value="Audi" id="Audi">Audi</option>
+                                <option value="Alpha" id="Alpha">Alpha</option>
+                                <option value="BMW" id="BMW">BMW</option>
+                                <option value="Bently" id="Bently">Bently</option>
+                                <option value="Chevrolet" id="Chevrolet">Chevrolet</option>
+                                <option value="Dodge" id="Dodge">Dodge</option>
+                                <option value="Ford" id="Ford">Ford</option>
+                                <option value="Honda" id="Honda">Honda</option>
+                                <option value="Kia" id="Kia">Kia</option>
+                                <option value="Lexus" id="Lexus">Lexus</option>
+                                <option value="Porsche" id="Porsche">Porsche</option>
+                                <option value="Tesla" id="Tesla">Tesla</option>
+                                <option value="Volvo" id="Volvo">Volvo</option>                                
+                            </select>
+                        <label htmlFor="model">Model:</label><input type="text" id="model" name="model" value={this.state.model} onChange={this.handleTextChange}></input>
+                        <label htmlFor="amount">Amount: $</label><input type="text" id="amount" name="amount" value={this.state.amount} onChange={this.handleTextChange}></input>
+                        <label htmlFor="description">Description:</label><input type="text" id="description" name="description" value={this.state.description} onChange={this.handleTextChange}></input>
+                        <label htmlFor="availability">Available?</label>
+                            <select id="availability" name="availability" value={this.state.availability} onChange={this.handleSelectChange}>
+                                <option value="yes" id="yes" selected>Yes</option>
+                                <option value="no" id="no">No</option>                                
+                            </select>
+                        
+                        
+                    </fieldset>
+                    <div className='button-center'>
+                            <br/>
+                            <Button variant="success" size="small" onClick={this.onClick}>Add Rental Car</Button>
+                    </div>
+                    </Modal>    
+                </div>
+            )
+        }
+        else{
+            return(
+                <div>
+                    <Button variant="success" size="small" onClick={this.openModal}><span className="glyphicon glyphicon-plus"></span></Button>
+                    <Modal
+                        isOpen={this.state.modalIsOpen}
+                        onAfterOpen={this.afterOpenModal}
+                        onRequestClose={this.closeModal}
+                        contentLabel="Add Expense"
+                        className="Modal">
+                    <div className='button-center'>
+                        <h3>{this.state.messageFromServer}</h3>
+                        <Link to={{pathname: '/', search: '' }} style={{ textDecoration: 'none' }}>
+                            <Button variant="success" size="mini" onClick={this.closeModal}>Close the Dialog</Button>
+                        </Link>
+                    </div>
+                    </Modal>
+                </div>
+            )
+        }
+    }
+}
+
+export default Add;
