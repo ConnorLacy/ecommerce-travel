04dd0cfa02ace51f10d148cec9f91b930da159a0
Lacy, Connor <lacyconnor1409@gmail.com>
2019-12-07 12:29:21 -0500

    Main app class

--- client/components/App.js
+++ client/components/App.js
@@ -1,25 +1,66 @@
 import React from 'react';
-import '../css/App.css';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Add from './Add'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+        selectedMake:'', selectedModel: '', data: []
+      };
+      this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+      this.getData(this, '');
+  }
+
+  componentDidUpdate(nextProps) {
+    this.getData(this, '');
+  }
+
+  getData(ev, model){
+      axios.get('/getAll?make=All&model=All')
+        .then(function(response) {
+          ev.setState({data: response.data});
+        });
+  }
+
+  render() {
+      return (
+        <div>
+          <Add selectedMake={this.state.selectedMake} selectedModel={this.state.selectedModel} />
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th className='button-col'>Year</th>
+                <th className='button-col'>Make</th>
+                <th className='button-col'>Model</th>
+                <th className='desc-col'>Description</th>
+                <th className='button-col'>Amount</th>
+                <th className='button-col'>Available</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.data.map(function(exp){
+                  return  <tr>
+                    <td className='counterCell'></td>
+                    <td className='button-col'>{exp.year}</td>
+                    <td className='button-col'>{exp.make}</td>
+                    <td className='button-col'>{exp.model}</td>
+                    <td className='desc-col'>{exp.description}</td>
+                    <td className='button-col'>{exp.amount}</td>
+                    <td className='button-col'>{exp.availability}</td>
+                    </tr>
+                })
+              }
+              </tbody>
+          </table>
+        </div>
+      );
+    }
 }
-
-export default App;
