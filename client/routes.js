74effab8f92ec1456a47188207fd2d879af0e5c7
Lacy, Connor <lacyconnor1409@gmail.com>
2019-12-11 14:56:51 -0500

    Added route to customer component

--- client/routes.js
+++ client/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Admin from './components/Admin';
 import Login from './components/Login'
+import Customer from './components/Customer';
 
 export const Routes = () => (
     <Switch>
       <Route exact path='/' component={Login} />
       <Route path='/admin' component={Admin} />
+      <Route path='/index' component={Customer} />
     </Switch>
 );
 export default Routes
