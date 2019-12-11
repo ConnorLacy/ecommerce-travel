5c9e2231422d00ea8ec111f88c983f316a58c4cf
Lacy, Connor <lacyconnor1409@gmail.com>
2019-12-11 15:49:17 -0500

    Changed styling techniques

--- client/components/Tile.js
+++ client/components/Tile.js
@@ -29,12 +29,13 @@ export default class Tile extends React.Component {
     }
 
     render(){
+        let availability = (this.state.available == 'yes') ? 'Available fadeInTotal' : 'Booked fadeInPartial';
         return (
-            <div className={(this.state.available) ? 'rental-tile available': 'rental-tile unavailable'} >
+            <div className={'rental-tile ' + availability } >
                 <img src="https://www.paylesscar.com/content/dam/cars/l/2017/nissan/2017-nissan-versa-sv-auto-sedan-blue_featured.png" width="250" height="250"/>
-                <h1>{this.state.year}{this.state.make}{this.state.model}</h1>
+                <h3>{this.state.year} {this.state.make} {this.state.model}</h3>
                 <p>{this.state.description}</p>
-                <h3>{this.state.available}</h3>
+                <h3>{availability.split(" ")[0]}</h3>
             </div>
         );
     }
