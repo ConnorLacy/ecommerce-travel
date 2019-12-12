0ed64dd060eb5adbe7d0cdb4f26cee824ddb1d33
Lacy, Connor <lacyconnor1409@gmail.com>
2019-12-11 21:16:02 -0500

    Initial commit of cart component

--- client/routes.js
+++ client/routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Admin from './components/Admin';
 import Login from './components/Login'
-import Customer from './components/Customer';
+import App from './components/App';
+import ShoppingCart from './components/ShoppingCart';
 
 export const Routes = () => (
     <Switch>
-      <Route exact path='/' component={Login} />
+      <Route exact path='/' component={App}/>
+      <Route path='/cart' component={ShoppingCart}/>        
+      <Route path='/login' component={Login}/>
       <Route path='/admin' component={Admin} />
-      <Route path='/index' component={Customer} />
     </Switch>
 );
 export default Routes
