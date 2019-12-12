3716c7c1840069f495d758cc1e16ff2b51158d32
Lacy, Connor <lacyconnor1409@gmail.com>
2019-12-11 21:17:40 -0500

    abstraction

--- client/components/Tile.js
+++ client/components/Tile.js
@@ -14,6 +14,31 @@ export default class Tile extends React.Component {
             amount: '',
             available: ''
         }
+        this.onClick = this.onClick.bind(this);
+        this.toggleSelectedClass = this.toggleSelectedClass.bind(this);
+    }
+
+    onClick(e){
+        this.toggleSelectedClass(e);
+    }
+
+    toggleSelectedClass(e){
+        let x = document.getElementsByClassName("selected");
+        console.log(x)
+        if(e.target.className.includes('Booked')){}
+        else{
+            if(e.target.className.includes("rental-tile") && !(e.target.className.includes("selected"))){
+                if(x.length > 0){
+                    for(let i=0; i<x.length; i++){
+                        x[i].classList.remove("selected");
+                    }
+                }
+                e.target.classList.add("selected");
+            }
+            else if(e.target.className.includes("rental-tile") && (e.target.className.includes("selected"))){
+                e.target.classList.remove("selected");
+            }
+        }
     }
     
     componentDidMount(){
@@ -31,7 +56,7 @@ export default class Tile extends React.Component {
     render(){
         let availability = (this.state.available == 'yes') ? 'Available fadeInTotal' : 'Booked fadeInPartial';
         return (
-            <div className={'rental-tile ' + availability } >
+            <div className={'rental-tile ' + availability } onClick={this.onClick}>
                 <img src="https://www.paylesscar.com/content/dam/cars/l/2017/nissan/2017-nissan-versa-sv-auto-sedan-blue_featured.png" width="250" height="250"/>
                 <h3>{this.state.year} {this.state.make} {this.state.model}</h3>
                 <p>{this.state.description}</p>
