33e07da07555b18aafe45dca4e9ce4146ae6fb97
Lacy, Connor <lacyconnor1409@gmail.com>
2019-12-12 12:41:04 -0500

    experimenting with lifecycles

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -21,6 +21,10 @@ export default class Admin extends React.Component {
       this.getData(this);
   }
 
+  componentWillUnmount(){
+    source.cancel('Operation aborted')
+  }
+
   componentDidUpdate() {
     this.getData(this);
   }
--- client/components/App.js
+++ client/components/App.js
@@ -6,6 +6,7 @@ import {withRouter} from 'react-router-dom'
 import {Link } from 'react-router-dom';
 import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import ShoppingCart from './ShoppingCart';
+require("babel-polyfill");
 const CancelToken = Axios.CancelToken;
 const source = CancelToken.source();
 
@@ -18,17 +19,25 @@ class App extends React.Component {
             isLoading: true,
             showCart: false
         };
-        this.getData = this.getData.bind(this);
+        // this.getData = this.getData.bind(this);
     }
     componentDidMount(){
         this.getData(this);
     }
 
+    componentWillUnmount(){
+        // source.cancel('Operation cancelled')
+    }
+
     componentDidUpdate(){
-        this.getData(this);
+       this.getData(this);
     }
 
     getData(App){
+        // let response = await Axios.get('/getAll?make=All&model=All');
+        // let { newData } = response.data;
+        // console.log(newData)
+        // this.setState({ data: newData});
         Axios.get('/getAll?make=All&model=All', {
             cancelToken:source.token
             }).then(response => {
@@ -50,7 +59,7 @@ class App extends React.Component {
             return(
                 <div className="customer-main">
                     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                        <Navbar.Brand>ParkWeb</Navbar.Brand>
+                        <Navbar.Brand><Link to="/"></Link>ParkWeb</Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="mr-auto">
@@ -69,7 +78,13 @@ class App extends React.Component {
                                     <Link to="/login" style={{ fontWeight: "Bold", color: 'white'}}>Login</Link>
                                 </Button>
                                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                                <Link to="/cart">
+                                <Link to={{
+                                    pathname: "/cart",
+                                    cartProps:{
+                                        rental: () => {
+                                            return document.getElementsByClassName("selected")
+                                        }
+                                    }}}>
                                     <Button variant="success">
                                         <span className="fa fa-shopping-cart">
                                             
@@ -78,27 +93,22 @@ class App extends React.Component {
                                 </Link>
                             </Form>
                         </Navbar.Collapse>
-                        </Navbar>
-                        {
-                            this.state.showCart ? (
-                                <ShoppingCart cartProp={this.state.cart}/>
-                            ) : (                       
-                                <div className="customer-container">
-                                <h1>Plan ahead and Travel worry-free</h1>
-                                    <div className="carousel">
-                                        {!this.state.isLoading ?
-                                            (
-                                                this.state.data.map(function(rental){
-                                                    return <Tile rentalProp={rental} key={rental._id}/>
-                                                })
-                                            ) 
-                                            : 
-                                            (
-                                                <p>Loading...</p>
-                                            )}
-                                    </div>
+                        </Navbar>                     
+                            <div className="customer-container">
+                            <h1>Plan ahead and Travel worry-free</h1>
+                                <div className="carousel">
+                                    {!this.state.isLoading ?
+                                        (
+                                            this.state.data.map(function(rental){
+                                                return <Tile rentalProp={rental} key={rental._id} hasButton={true}/>
+                                            })
+                                        ) 
+                                        : 
+                                        (
+                                            <p>Loading...</p>
+                                        )}
                                 </div>
-                                )
+                            </div>
                         }
                 </div>
             );
